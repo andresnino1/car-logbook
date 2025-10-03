cd12ff467a8e5eff6549972fc79c191c107f709f
andresnino@gmail.com <andresnino@gmail.com>
2025-10-03 08:37:47 +0000

    Created Logbook; Edited settings and theme/parameters.yaml; Deleted Form1

--- client_code/Logbook/__init__.py
+++ client_code/Logbook/__init__.py
@@ -1,13 +1,14 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import LogbookTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import m3.components as m3
 
-class Form1(Form1Template):
+class Logbook(LogbookTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
+    self.button_send.role = None
+    self.button_send.appearance 
+    
